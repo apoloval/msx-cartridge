787ee9e9dfe38639a07e96aadf21d2be6d11acf3
Alvaro Polo <apoloval@gmail.com>
2016-08-07 13:18:53 +0200

    Adapt rom burner cli to support 20 and 40 SST rom models

--- rom-burner/cli/rbc.rb
+++ rom-burner/cli/rbc.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 require "serialport"
 
-MAX_SECTORS = 32
+MAX_SECTORS = 128
 
 def read_handshake(serial)
   print "\tReceiving handshake from device... "
@@ -45,7 +45,7 @@ def dump_rom(output_file, sectors)
     File.open(output_file, "wb") { |file|
       for sector in sectors
         sector_addr = sector << 12
-        print "\tReading sector at address 0x%.4x... " % sector_addr
+        print "\tReading sector %d at address 0x%.5x... " % [sector, sector_addr]
         sector = read_sector(serial, sector)
         print "Done\n"
         file.write(sector)
@@ -60,7 +60,7 @@ def erase_sectors(sectors)
     read_handshake(serial)
     for sector in sectors
       sector_addr = sector << 12
-      print "\tErasing sector at address 0x%.4x... " % sector_addr
+      print "\tErasing sector %d at address 0x%.5x... " % [sector, sector_addr]
       errors = erase_sector(serial, sector)
       if errors == 0
         print "Done\n"
@@ -79,7 +79,7 @@ def burn_rom(input_file, initial_sector)
       sector = initial_sector
       until file.eof?
         sector_addr = sector << 12
-        print "\tWriting sector at address 0x%.4x... " % sector_addr
+        print "\tWriting sector %d at address 0x%.5x... " % [sector, sector_addr]
         data = file.read(4096)
         if data.length < 4096
           print "(too small sector, padding with zeroes) "
@@ -97,7 +97,17 @@ def report_error(error)
   abort "Error: #{error}\n" +
     "Usage: rbc dump <output file> [<initial sector>] [<sector count>]\n" +
     "       rbc erase [<initial sector>] [<sector count>]\n" +
-    "       rbc burn <input file> [<initial sector>]\n"
+    "       rbc burn <input file> [<initial sector>]\n" + 
+    "\n" +
+    "Parameters:\n" +
+    "   <input file>      The file where burned data would be read from\n" +
+    "   <output file>     The file where dumped data would be stored\n" +
+    "   <initial sector>  The first sector the operation would be applied.\n" +
+    "                     The first sector is 0. The last sector for SST39F010/20/40\n" +
+    "                     is 31, 63 and 127, respectively.\n" +
+    "   <sector count>    The number of sectors that will be affected by the operation.\n" +
+    "                     Note SST39F010/20/40 has 32, 64 and 128 sectors, respectively\n" +
+    "\n"
 end
 
 def get_arg(i)
@@ -108,8 +118,8 @@ def get_arg_opt(i, default)
   ARGV[i].nil? ? default : ARGV[i]
 end
 
-def get_arg_in_range(i, range, default)
-  val = get_arg_opt(i, default).to_i
+def get_arg_in_range(i, range, default=nil)
+  val = default.nil? ? get_arg(i).to_i : get_arg_opt(i, default).to_i
   range.include?(val) ? val : report_error("value #{val} is out of range")
 end
 
